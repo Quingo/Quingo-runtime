935be604c28b3c2acdb347295530a816420d487c
LT <listenwetnessluo@163.com>
2021-12-25 14:49:32 +0800

    Add the case of ghz.

---
+++ src/examples/ghz/host.py
@@ -0,0 +1,23 @@
+import logging
+from quingo import quingo_interface as qi
+from pathlib import Path
+
+# qi.set_verbose(True)
+# qi.set_log_level(logging.DEBUG)
+qi.set_compiler('mlir')
+
+if qi.connect_backend('pyqcisim_quantumsim') is False:
+    exit(-1)
+
+qu_file = Path(__file__).parent / "kernel.qu"
+
+
+def routine(circ_name, num_shots=1, num_qubits=1):
+    qi.set_num_shots(num_shots)
+    if not qi.call_quingo(qu_file, circ_name, num_qubits):
+        print("Failed to call {}".format(circ_name))
+    print("The result of {} is:".format(circ_name))
+    print(qi.read_result())
+
+
+routine("ghz", 5, 2)
