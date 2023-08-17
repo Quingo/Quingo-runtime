f0e1d55b3afb18ceffbb93ce88d60f3a40fdb988
Xiang Fu <xiangfu@quanta.org.cn>
2023-08-17 16:41:07 +0800

    add Qisa into quingo __init__

--- src/quingo/__init__.py
+++ src/quingo/__init__.py
@@ -2,4 +2,5 @@ from quingo.core.exe_config import ExeConfig, ExeMode
 from quingo.core.compile import compile
 from quingo.core.manager import execute, call
 from quingo.core.quingo_task import Quingo_task
-from quingo.backend.backend_hub import BackendType
+from quingo.backend.backend_hub import *
+from quingo.backend.qisa import *
