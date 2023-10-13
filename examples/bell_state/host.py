792b73957c56f2e71525eb560042ef769befd514
刘定东 <3081545786@qq.com>
2023-10-13 19:54:46 +0800

    🧪 test: add examples

--- examples/bell_state/host.py
+++ examples/bell_state/host.py
@@ -5,21 +5,31 @@ from pathlib import Path
 qu_file = Path(__file__).parent / "kernel.qu"
 
 
-def routine(circ_name, num_shots=1):
+def SimFinalResult(circ_name, num_shots=1):
     task = Quingo_task(qu_file, circ_name)
-    cfg = ExeConfig(ExeMode.SimStateVector, 3)
-    # exemod 1
-    res = call(task, (), BackendType.DQCSIM_QUANTUMSIM, cfg)
-    # exemod2
-    qasm_fn = compile(task, params=())
+    cfg = ExeConfig(ExeMode.SimFinalResult, num_shots)
+    qasm_fn = compile(task, params=(), config_file="")
+    res = execute(qasm_fn, BackendType.DQCSIM_TEQUILA, cfg)  # QuantumSim, SymQC
+    print("sim res: ", res)
+
+
+def SimStateVector(circ_name):
+    task = Quingo_task(qu_file, circ_name)
+    cfg = ExeConfig(ExeMode.SimStateVector)
+    qasm_fn = compile(task, params=(), config_file="")
     res = execute(qasm_fn, BackendType.DQCSIM_QUANTUMSIM, cfg)
     print("sim res for bell state is:")
     print("classical:", res["classical"])
     print("quantum:", res["quantum"])
-    # res = execute(qasm_fn, BackendType.DQCSIM_TEQUILA, cfg)
-    # print("sim res: ", res)
-    # res = execute(qasm_fn, BackendType.SYMQC, cfg)
-    # print("sim res: ", res)
 
 
-routine("bell_state", 10)
+def routine(circ_name, num_shots=1):
+    task = Quingo_task(qu_file, circ_name)
+    cfg = ExeConfig(ExeMode.SimFinalResult, num_shots)
+    res = call(task, (), BackendType.SYMQC, cfg)
+    print("sim res: ", res)
+
+
+# SimFinalResult("bell_state", 10)
+# SimStateVector("bell_state")
+routine("bell_state", 5)
--- examples/ctrl_qubit/host.py
+++ examples/ctrl_qubit/host.py
@@ -7,7 +7,7 @@ qu_file = Path(__file__).parent / "kernel.qu"
 
 def routine(circ_name, num_shots=1):
     task = Quingo_task(qu_file, circ_name)
-    cfg = ExeConfig(ExeMode.SimStateVector, 10)
+    cfg = ExeConfig(ExeMode.SimFinalResult, 10)
     # exemod 1
     res = call(task, (), BackendType.DQCSIM_QUANTUMSIM, cfg)
     # exemod2
