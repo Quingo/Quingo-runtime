31ff3fecd09c04f209f6949597f68a3509908643
Xiang Fu <xiangfu@quanta.org.cn>
2025-08-07 15:25:38 +0800

    更新版本号至0.3.5，新增.gitignore和config/README.md文件，调整测试文件路径以使用token文件。

--- unittest/backends/test_tianyan.py
+++ unittest/backends/test_tianyan.py
@@ -4,9 +4,11 @@ from quingo.backend.backend_hub import BackendType
 from quingo.backend.qisa import Qisa
 from quingo.backend.tianyan import ZDXLZ_Tianyan
 from quingo.core.exe_config import *
+from quingo.global_config import qgrtsys_repo_dir
 
 cur_dir = Path(__file__).parent
-qcis_fn = cur_dir / ".." / "test_qcis" / "bell_cz.qcis"
+qcis_fn = qgrtsys_repo_dir / "unittest" / "test_qcis" / "bell_cz.qcis"
+token_fn = qgrtsys_repo_dir / "config" / "tianyan.tokens"
 
 
 def test_execute():
@@ -15,7 +17,7 @@ def test_execute():
     exe_config = ExeConfig(
         mode=ExeMode.RealMachine,
         num_shots=10,
-        qcloud_platform_login_key="95hoNFTHmKqtXNi5gGUAjS7As2l8xm5Ymze1DHZFJx4=",
+        qcloud_platform_login_key=token_fn.read_text().strip(),
         qcloud_machine_name="tianyan_sw",
     )
     results = platform.execute(exe_config)
